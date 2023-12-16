cd33f5fb898c8313e441f6d3b086c70ed279e26c
Haoran Yin <335785626@qq.com>
2023-12-17 00:43:40 +0800

    实现了缓存菜品和套餐的代码；实现了购物车的增删查的代码

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,9 +11,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * 菜品管理
@@ -27,11 +29,18 @@ public class DishController {
     @Autowired
     private DishService dishService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @PostMapping
     @ApiOperation("新增菜品")
     public Result save(@RequestBody DishDTO dishDTO){
         log.info("新增菜品:{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
+
+        //清理缓存数据
+        String key="dish_"+dishDTO.getCategoryId();
+        cleanCache("key");
 
         return Result.success();
     }
@@ -59,6 +68,9 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("菜品批量删除:{}",ids);
         dishService.deleteBatch(ids);
+
+        //将所有菜品缓存都清理掉
+        cleanCache("dish_*");
 
         return Result.success();
     }
@@ -88,6 +100,9 @@ public class DishController {
         log.info("修改菜品:{}",dishDTO);
         dishService.updateWithFlavor(dishDTO);
 
+        //将所有菜品缓存都清理掉
+        cleanCache("dish_*");
+
         return Result.success();
     }
 
@@ -102,6 +117,9 @@ public class DishController {
     public Result startOrStop(@PathVariable Integer status,Long id){
         log.info("启用禁用菜品信息:{},{}",status,id);
         dishService.startOrStop(status,id);
+
+        //将所有菜品缓存都清理掉
+        cleanCache("dish_*");
 
         return Result.success();
     }
@@ -121,5 +139,13 @@ public class DishController {
         return Result.success(dishList);
     }
 
+    /**
+     * 清理Redis缓存数据
+     * @param pattern
+     */
+    private void cleanCache(String pattern){
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
+    }
 
 }
---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,138 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Service
+@Slf4j
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+    @Override
+    public void addShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+        //判断当前加入购物车的商品是否已经存在
+        ShoppingCart shoppingCart=new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO,shoppingCart);
+        Long userId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(userId);
+
+        List<ShoppingCart> list=shoppingCartMapper.list(shoppingCart);
+
+        //若已经存在，则数量+1即可
+        if(list!=null && !list.isEmpty()){
+            ShoppingCart cart = list.get(0);
+            cart.setNumber(cart.getNumber()+1);
+            shoppingCartMapper.updateNumberById(cart);
+        }else {
+            //若不存在，则需要插入一条数据
+            //判断本次添加到购物车的是套餐还是菜品
+            Long dishId = shoppingCartDTO.getDishId();
+            if(dishId!=null){
+                //本次添加到购物车的是菜品
+                Dish dish = dishMapper.getById(dishId);
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setImage(dish.getImage());
+                shoppingCart.setAmount(dish.getPrice());
+
+
+            }else {
+                //本次添加到购物车的是套餐
+                Long setmealId = shoppingCart.getSetmealId();
+                Setmeal setmeal = setmealMapper.getById(setmealId);
+
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setImage(setmeal.getImage());
+                shoppingCart.setAmount(setmeal.getPrice());
+
+
+
+            }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+
+            shoppingCartMapper.insert(shoppingCart);
+
+
+        }
+
+
+    }
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        //获取当前微信用户的id
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart=ShoppingCart.builder()
+                        .userId(userId)
+                        .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+    }
+
+    /**
+     * 清空购物车
+     */
+    @Override
+    public void cleanShoppingCart() {
+        Long userId = BaseContext.getCurrentId();
+        shoppingCartMapper.deleteByUserId(userId);
+
+    }
+
+    /**
+     * 删除购物车中的一件商品
+     * @param shoppingCartDTO
+     */
+    @Override
+    public void subShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+        ShoppingCart shoppingCart=new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO,shoppingCart);
+        Long userId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(userId);
+
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+
+        if(list!=null && !list.isEmpty()){
+            ShoppingCart cart=list.get(0);
+            Integer number = cart.getNumber();
+            if(number==1){
+                //删除这条字段
+                shoppingCartMapper.deleteById(cart.getId());
+            }else {
+                //将对应的套餐或菜品的数量减一
+                cart.setNumber(cart.getNumber()-1);
+                shoppingCartMapper.updateNumberById(cart);
+            }
+        }
+
+    }
+}
