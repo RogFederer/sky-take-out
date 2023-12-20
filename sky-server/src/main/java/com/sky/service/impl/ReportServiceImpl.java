2ca248368bce4b5ad41ecd87c07b7ddceccb983c
Haoran Yin <335785626@qq.com>
2023-12-20 12:11:40 +0800

    实现了用户数量统计的代码

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,8 +2,10 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,8 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private UserMapper userMapper;
 
     /**
      * 统计指定时间区间内的营业额数量
@@ -68,5 +72,55 @@ public class ReportServiceImpl implements ReportService {
                 .build();
 
         return turnoverReportVO;
+    }
+
+    /**
+     * 统计指定时间区间内的用户数量
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        List<LocalDate> dateList=getDateList(begin, end);
+        //存放每天的新增用户数量和总用户数量
+        List<Integer> newUserList=new ArrayList<>();
+        List<Integer> totalUserList=new ArrayList<>();
+
+        for(LocalDate date:dateList){
+            LocalDateTime beginTime=LocalDateTime.of(date,LocalTime.MIN);
+            LocalDateTime endTime=LocalDateTime.of(date,LocalTime.MAX);
+            Map map=new HashMap<>();
+            map.put("end",endTime);
+            Integer totalUser=userMapper.countByMap(map);
+            map.put("begin",beginTime);
+            Integer newUser=userMapper.countByMap(map);
+            newUserList.add(newUser);
+            totalUserList.add(totalUser);
+
+        }
+
+        String dateListStr=StringUtils.join(dateList,",");
+        String newUserListStr=StringUtils.join(newUserList,",");
+        String totalUserListStr=StringUtils.join(totalUserList,",");
+
+        return UserReportVO.builder()
+                .dateList(dateListStr)
+                .newUserList(newUserListStr)
+                .totalUserList(totalUserListStr)
+                .build();
+    }
+
+    private List<LocalDate> getDateList(LocalDate begin, LocalDate end){
+        //当前集合用于存放时间区间内的所有日期
+        List<LocalDate> dateList=new ArrayList<>();
+        //日期计算
+        dateList.add(begin);
+        while(!begin.equals(end)){
+            begin=begin.plusDays(1);
+            dateList.add(begin);
+        }
+        //将日期集合拼接成字符串
+        return dateList;
     }
 }
